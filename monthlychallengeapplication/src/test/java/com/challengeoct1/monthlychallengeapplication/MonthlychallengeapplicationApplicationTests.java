6398df98c4062e11aa7b4278f1e2c1d276c50cd8
sanjive05 <sanjivesubramaniam4154@gmail.com>
2024-06-07 00:09:06 +0530

    project setup completed..

---
+++ monthlychallengeapplication/src/test/java/com/challengeoct1/monthlychallengeapplication/MonthlychallengeapplicationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.challengeoct1.monthlychallengeapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MonthlychallengeapplicationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
